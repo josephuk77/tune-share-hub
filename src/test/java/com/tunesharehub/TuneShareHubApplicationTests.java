19661dcce646aed4918b3a928fed861d5ff4473e
josephuk77 <josephuk77@gmail.com>
2026-04-27 09:15:49 +0900

    fix(config): 리뷰 지적 반영

--- src/test/java/com/tunesharehub/TuneShareHubApplicationTests.java
+++ src/test/java/com/tunesharehub/TuneShareHubApplicationTests.java
@@ -2,11 +2,10 @@ package com.tunesharehub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-                + "org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration"
-})
+@ActiveProfiles("test")
+@SpringBootTest
 class TuneShareHubApplicationTests {
 
     @Test
